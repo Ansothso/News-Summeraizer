c90392a126e5718546ba4184b6650197f5796c7e
Samu Khodak <57069695+salut0samu@users.noreply.github.com>
2023-01-27 00:34:54 +0100

    added contact form and hyperlink to contact form

--- NewsSummerizer/frontend/pages/06_Help.py
+++ NewsSummerizer/frontend/pages/06_Help.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Help")
-st.write("If you have problems read through the tips provided here.")
+st.write("If you have problems read through the tips provided here. For further questions, you can [contact us](http://localhost:8506/About_us).")
