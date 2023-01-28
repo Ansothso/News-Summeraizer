1d1cf9b48f87b2f02e5666aa85bfbd3887e16734
sei-bas <s.basha@stud.uni-frankfurt.de>
2023-01-28 14:38:14 +0100

    fixed typo in the folder name

--- NewsSummerizer/frontend/pages/05_Own-Summary.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Creat your own Summary")
-st.write("You are able to input your own text into the textfield. By pressing the button you will generate your own summary.")
--- NewsSummerizer/frontend/pages/06_Help.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Help")
-st.write("If you have problems read through the tips provided here. For further questions, you can [contact us](http://localhost:8506/About_us).")
