9412bd3d3d418783a67a860a8eb199677500bdee
sei-bas <s.basha@stud.uni-frankfurt.de>
2023-02-12 17:59:03 +0100

    fix merge problem

--- NewsSummarizer/frontend/01_Home.py
+++ NewsSummarizer/frontend/01_Home.py
@@ -11,15 +11,12 @@ st.set_page_config(
 st.title("News-Summarizer")
 #st.sidebar.success("Select a Page.")
 
-<<<<<<< HEAD
 if "test_in" not in st.session_state:
    st.session_state["test_in"] = ""
-=======
 st.write("Welcome to the News-Summarizer")
 st.write("This Application will summarize news of the previous day for you to read. You can read a random summary in our Top-News section. If you want to read news of a certain category check ot the category section.")
 st.write("You will also be able to create your own summary. In our Own-Summary section you can insert a news-article to have it summarized for you. In addition, you will be able to download your summary as a .txt file to share with others.")
 st.write("On our Homepage you can read a more general summary of the previous day.")
->>>>>>> e7fde4add5bb523b87be2c89367fbcc9c70b9169
 
 test_in = st.text_input("This test", st.session_state["test_in"])
 submit = st.button("Klick")
