11f05605db4635ee7422d63d7ff0d7034babd261
sei-bas <s.basha@stud.uni-frankfurt.de>
2023-03-11 23:30:13 +0100

    Further corrections of the summary generator. Proofread and edited the text of the help page.

--- NewsSummarizer/frontend/pages/05_Summary_Generator.py
+++ NewsSummarizer/frontend/pages/05_Summary_Generator.py
@@ -9,11 +9,11 @@ st.set_page_config(
     page_icon=im
 )
 
-st.title("Create your own Summary")
+st.title("Create your own summary")
 st.write('You can input your own text into the textfield. By clicking the "Generate" button, a summary will be generated. This summary can also be downloaded as a .txt file.')
     
 wiedtest = ""
-testatiu = st.text_area("Input your article to summarize.", height=300)
+testatiu = st.text_area("Input your article text to summarize:", height=300)
 submit2 = st.button("Generate")
 if submit2:
     tee2 = testatiu.replace('\n', ' ')
@@ -21,4 +21,4 @@ if submit2:
     #st.session_state["test_in"] = test_in
     st.write(wiedtest)
 
-st.download_button("Click to download", wiedtest)
+st.download_button("Download", wiedtest)
