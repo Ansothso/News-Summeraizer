14814567b5484a11ee1a2a855b5cfa876dc60ec3
Lily Djami <mlily_d@proton.me>
2023-03-11 15:56:57 +0100

    added cropped banner images

--- NewsSummarizer/frontend/pages/01_Top-News.py
+++ NewsSummarizer/frontend/pages/01_Top-News.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from PIL import Image
 
+image = Image.open('images/banner_topnews.png')
 st.title("Test-News")
-st.write("Read the latest news.")
-image = Image.open('stock_image.jpg')
+st.image(image)
+st.caption("Image by [Cottonbro Studio](https://www.pexels.com/de-de/foto/espresso-fruhstuck-nachrichten-zeitung-3944425/)")
 
-st.image(image, caption='This is a stock image Created in create.swf/Walfas')
+st.write("Read the latest news.")
+
+
+
 st.write("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
 
 col1, col2 = st.columns(2)
